d08152821b5cf4fbc8c6d3129a240938216ef435
SdneyFernandes <fsidney987@gmail.com>
2025-01-25 18:39:43 -0300

    Entity, ORM, H2

---
+++ src/main/java/br/com/dslist/entities/Game.java
@@ -0,0 +1,130 @@
+package br.com.dslist.entities;
+
+
+import java.util.Objects;
+
+import jakarta.persistence.*;
+
+/**
+ * @author fsdney
+ */
+
+@Entity
+@Table(name = "tb_game")
+public class Game {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String genre;
+	
+	@Column(name = "game_year")//year é uma palavra reservada do sql , por isso editar o nome da tabela
+	private Integer year;
+	private String platforms;
+	private String imgUrl;
+	private String shortDescription;
+	private String longDescription;
+	
+	public Game() {
+		
+	}
+
+	public Game(Long id, String title, String genre, Integer year, String platforms, String imgUrl,
+			String shortDescription, String longDescription) {
+		this.id = id;
+		this.title = title;
+		this.genre = genre;
+		this.year = year;
+		this.platforms = platforms;
+		this.imgUrl = imgUrl;
+		this.shortDescription = shortDescription;
+		this.longDescription = longDescription;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getPlatforms() {
+		return platforms;
+	}
+
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getLongDescription() {
+		return longDescription;
+	}
+
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+
+}
